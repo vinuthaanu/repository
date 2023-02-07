9b217fe0b82a67a9d522132f1538386bcd5694d1
vinuthaanu <vinuthaanu22@gmail.com>
2023-02-07 21:58:16 +0530

    nmv

--- src/main/java/mainapplication.java
+++ src/main/java/mainapplication.java
@@ -3,6 +3,6 @@ public class mainapplication
     public static void main(String[] args) {
 
         System.out.println(" hh");
-        System.out.println("his is my first class");
+         System.out.println("his is my first class");
     }
 }

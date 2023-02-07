6dbaac57ffddf3b68d92c2cc5c93b990318e37ff
vinuthaanu <vinuthaanu22@gmail.com>
2023-02-07 21:50:59 +0530

    nm

--- src/main/java/mainapplication.java
+++ src/main/java/mainapplication.java
@@ -1,6 +1,6 @@
 public class mainapplication
 {
     public static void main(String[] args) {
-        System.out.println("this is my first class");
+        System.out.println("his is my first class");
     }
 }
